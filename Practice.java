b81ae9c0ae0a86236ae9f0a27cb1d5f39830b1d4
sayan04052001 <ghoshsayan358@gmail.com>
2024-05-20 20:29:58 +0530

    new code

--- Practice.java
+++ Practice.java
@@ -1,3 +1,11 @@
 interface Calc{
 	int cal(int a, int b);
 }
+if(str.length() == 3)
+return str.substring(0, 3).equals("bad");
+  
+if(str.length() >= 4)
+return str.substring(0, 3).equals("bad") || 
+	str.substring(1, 4).equals("bad");
+			
+return false
